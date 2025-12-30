e7c48405943a355aa89f6763d73fc71fee373a49
Tomasz Omeljaniuk <tomekomel@gmail.com>
2025-12-30 16:08:04 +0100

    feat: Circular deps debugging

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
+import { COFFEES_DATA_SOURCE } from './coffees.const';
 
 @Module({
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [CoffeesService, { provide: COFFEES_DATA_SOURCE, useValue: [] }],
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
+import { COFFEES_DATA_SOURCE } from './coffees.const';
+
+export interface CoffeesDataSource {
+  [index: number]: Coffee;
+}
 
 @Injectable()
 export class CoffeesService {
+  constructor(@Inject(COFFEES_DATA_SOURCE) dataSource: CoffeesDataSource) {}
+
   create(createCoffeeDto: CreateCoffeeDto) {
     return 'This action adds a new coffee';
   }
