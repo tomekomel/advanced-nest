e93db1ad82129a8c8a50a6c90f266dbcdf5bb264
Tomasz Omeljaniuk <tomekomel@gmail.com>
2025-12-30 16:43:52 +0100

    feat: Injecting value/consts with Inject decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CoffeesService } from './coffees.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+} from '@nestjs/common';
+import { CoffeesDataSource, CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { COFFEES_DATA_SOURCE } from './coffees.const';
 
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(
+    private readonly coffeesService: CoffeesService,
+    @Inject(COFFEES_DATA_SOURCE)
+    private readonly coffeesDataSource: CoffeesDataSource,
+  ) {}
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
